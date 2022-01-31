17ae45d996b6425694f551abf57c29436567fd6f
Collin McMahon <collin@yellingmule.com>
2022-01-30 22:50:18 -0500

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bd10f924567ac183ea495618b5ceb2db'),
+    },
+  },
+});
